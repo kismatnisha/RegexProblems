fd80642d3c6d7f0111ccc88f97657fab724e156e
qismat <qismatnisha6243@gmail.com>
2022-12-31 22:21:37 +0530

    [qismat] Add : UC2-ValidationLastName

--- UserRegistrationProblem/UserRegistrationProblem/Program.cs
+++ UserRegistrationProblem/UserRegistrationProblem/Program.cs
@@ -4,11 +4,16 @@
     {
         static void Main(string[] args)
         {
+
             Console.WriteLine("Welcome to User Registration Problems Statement Using REGEX");
             UserValidation userValidation = new UserValidation();
             Console.WriteLine("Enter The First Name: ");
             string firstname = Console.ReadLine();
             userValidation.ValidationFirstName(firstname);
+
+            Console.WriteLine("Enter The Last Name: ");
+            string lastname = Console.ReadLine();
+            userValidation.ValidationLastName(lastname);
         }
     }
 }
